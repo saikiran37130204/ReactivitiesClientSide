32353692555afa99a52602873d215b7541fac5d5
saikiran37130204 <saikirankondreddy.1@gmail.com>
2025-02-06 22:09:12 -0600

    implemented the attendance feature

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,44 +1,53 @@
-import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
-import { /* useDispatch,*/ useSelector } from "react-redux";
-import { RootState } from "../../../redux/store";
 import { format } from "date-fns";
-// import { SyntheticEvent, useState } from "react";
-// import { deleteActivityRequest } from "../../../redux/Slice/ActivitiesSlice";
+import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
   activity: Activity;
 }
 export default function ActivityListItem({ activity }: Props) {
-  //const dispatch = useDispatch();
-
-  const { activities } = useSelector((state: RootState) => state.activities);
-
-  console.log("Activities By Date:", activities);
-
-  //const [target, setTarget] = useState("");
-
-  // function handleActivityDelete(
-  //   e: SyntheticEvent<HTMLButtonElement>,
-  //   id: string
-  // ) {
-  //   setTarget(e.currentTarget.name);
-
-  //   dispatch(deleteActivityRequest(id));
-  // }
-
   return (
     <Segment.Group>
       <Segment>
+        {activity.isCancelled && (
+          <Label
+            attached="top"
+            color="red"
+            content="Cancelled"
+            style={{ textAlign: "center" }}
+          />
+        )}
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src="/Asserts/user.png" />
+            <Item.Image
+              style={{ marginBottom: 5 }}
+              size="tiny"
+              circular
+              src="/Asserts/user.png"
+            />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted by Bob</Item.Description>
+              <Item.Description>
+                Hosted by {activity.host?.displayName || "unknown host"}
+              </Item.Description>
+              {activity.isHost && (
+                <Item.Description>
+                  <Label basic color="orange">
+                    you are hosting this activity
+                  </Label>
+                </Item.Description>
+              )}
+              {activity.isGoing && !activity.isHost && (
+                <Item.Description>
+                  <Label basic color="green">
+                    you are going to this activity
+                  </Label>
+                </Item.Description>
+              )}
             </Item.Content>
           </Item>
         </Item.Group>
@@ -46,12 +55,14 @@ export default function ActivityListItem({ activity }: Props) {
       <Segment>
         <span>
           <Icon name="clock" />
-          {format(activity.date!,'dd MMM yyyy h:mm aa')}
+          {format(activity.date!, "dd MMM yyyy h:mm aa")}
           <Icon name="marker" />
           {activity.venue}
         </span>
       </Segment>
-      <Segment secondary>Attendees go here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendee attendees={activity.attendees!} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
